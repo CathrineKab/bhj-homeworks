73a4093da45090ecf9cc43084c330529a629c246
Екатерина Кабуя <katusha.pochta@gmail.com>
2022-07-14 17:49:23 +0300

    Modified tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,24 @@
+const hastooltip = Array.from(document.querySelectorAll('.has-tooltip'));
+
+hastooltip.forEach((el) => {
+    const tooltipDiv = document.createElement('div');
+    tooltipDiv.innerText = `${el.title}`;
+    el.addEventListener("click", (e) => {
+        e.preventDefault();
+        tooltipDiv.classList.add("tooltip");
+        tooltipDiv.setAttribute(`style`, `left: ${el.getBoundingClientRect().left}px; top: ${el.getBoundingClientRect().top + 16}px`);
+        
+        el.insertAdjacentElement('beforeBegin', tooltipDiv);
+        const tooltipActive = Array.from(document.querySelectorAll(".tooltip_active"));
+        const findElem = tooltipActive.find(tooltipDiv => tooltipDiv.classList.contains("tooltip_active"));
+        console.log(findElem);
+        if (findElem === undefined) {
+            tooltipDiv.classList.add("tooltip_active");
+        } else {
+            if (findElem !== tooltipDiv) {
+                tooltipDiv.classList.add("tooltip_active");
+            }
+            findElem.classList.remove("tooltip_active");
+        }
+    })
+})
