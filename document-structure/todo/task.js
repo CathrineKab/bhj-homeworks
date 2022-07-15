39d95dfcad6df89a0050f929ca0dd83d44cdcd1c
Екатерина Кабуя <katusha.pochta@gmail.com>
2022-07-15 13:42:11 +0300

    Modified todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,24 @@
+const input = document.getElementById("task__input");
+const add = document.getElementById("tasks__add");
+const list = document.querySelector('.tasks__list');
+
+add.addEventListener('click', addTask);
+
+function addTask() {
+    event.preventDefault();
+    if (input.value != " ") {
+        list.insertAdjacentHTML(`beforeEnd`,
+        `<div class = "task">
+        <div class = "task__title"> ${input.value} </div> 
+        <a href = "#"
+        class = "task__remove">&times;</a> </div>`
+        );
+        input.value = ``;
+    }
+
+
+    const task = Array.from(document.querySelectorAll(".task__remove"));
+    task[task.length - 1].addEventListener("click", () => {
+        list.removeChild(task[task.length - 1].closest(".task"));
+    })
+}
